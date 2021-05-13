05649cec5620f72c52e08136b67721e22674cd02
yewon-Noh <yewo2nn16@gmail.com>
2021-05-13 16:17:12 +0900

    실패작

--- src/MainForm.java
+++ src/MainForm.java
@@ -1,9 +1,18 @@
-import javax.swing.JFrame;
+import java.awt.Color;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class MainForm extends JFrame {
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class MainForm extends JFrame implements ActionListener {
 	
 	private String title = "INHA AIR";
 	private int width = 1120, height = 770;
+	
+	private JPanel main, sub;
+	private JButton btn1;
 	
 	public MainForm() {
 		setTitle(title);
@@ -12,13 +21,57 @@ public class MainForm extends JFrame {
 		setLocationRelativeTo(this);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		
+		setLayout(null);
+		
+		mainPan();
+		
+		subPan();
 		
 		setVisible(true);
 	}
-	
-	
+
+
+	private void mainPan() {
+		main = new JPanel();
+		main.setLayout(null);
+		main.setSize(1120, 80);
+		main.setLocation(0, 0);
+		main.setBackground(Color.WHITE);
+		
+		btn1 = new JButton("INHA");
+		btn1.setBackground(Color.white);
+		btn1.setSize(80, 80);
+		btn1.setLocation(50, 0);
+		btn1.addActionListener(this);
+		main.add(btn1);
+		
+		add(main);
+	}
+
+	private void subPan() {
+		sub = new JPanel();
+		sub.setLayout(null);
+		sub.setSize(1120, 300);
+		sub.setLocation(0, 0);
+		sub.setBackground(Color.yellow);
+		
+//		add(sub);
+	}
+
 	public static void main(String[] args) {
 		new MainForm();
+	}
+
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		Object obj = e.getSource();
+		
+		if(obj == btn1)
+		{
+			main.setVisible(false);
+			add(sub);
+		}
 	}
 
 }
