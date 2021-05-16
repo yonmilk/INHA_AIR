b5a31d939ac03355852abdaffc0a9333a8ae8b5f
yewon-Noh <yewo2nn16@gmail.com>
2021-05-16 19:34:00 +0900

    상단 디자인 변경

--- src/MainForm.java
+++ src/MainForm.java
@@ -17,12 +17,15 @@ public class MainForm extends JFrame implements ActionListener {
 	
 	// 폰트
 	Font fontGothic = new Font("Gothic", Font.BOLD, 20);
-	Font fontNanumGothic = new Font("NanumGothic", Font.BOLD, 20);
+	Font fontNanumGothic = new Font("NanumGothic", Font.BOLD, 18);
 	Font fontArial = new Font("Arial", Font.PLAIN, 12);
 	
 	// 상단 메뉴 관련 
 	private JPanel pnTOP, pnMenu;
 	private JButton btnLogo, btnBook, btnAirport, btnInFight, btnMyPage, btnLogin;
+	
+	// 로그인 프레임
+	private loginForm login;
 	
 	public MainForm() {
 		setTitle(title);
@@ -61,14 +64,17 @@ public class MainForm extends JFrame implements ActionListener {
 		// 메뉴 선택 판넬
 		pnMenu = new JPanel();
 		pnMenu.setLayout(null);
-		pnMenu.setSize(700, 70);
-		pnMenu.setLocation(250, 5);
+//		pnMenu.setSize(700, 70);
+		pnMenu.setSize(1000, 50);
+//		pnMenu.setLocation(250, 5);
+		pnMenu.setLocation(70, 40);
 //		pnMenu.setBackground(Color.LIGHT_GRAY);
 		
 		// 메뉴 버튼
 		btnBook = new JButton("예매");			// 예매 버튼 생성
-		btnBook.setSize(150, 70);				// 버튼 사이즈 지정
-		btnBook.setLocation(10, 0);				// 버튼 위치 지정
+		btnBook.setSize(150, 40);				// 버튼 사이즈 지정
+//		btnBook.setLocation(10, 0);				// 버튼 위치 지정
+		btnBook.setLocation(200, 0);				// 버튼 위치 지정
 		btnBook.addActionListener(this);
 		btnBook.setFont(fontNanumGothic);		// 버튼 폰트 설정
 //		btnBook.setBackground(Color.LIGHT_GRAY);	// 버튼 배경색 설정
@@ -77,24 +83,27 @@ public class MainForm extends JFrame implements ActionListener {
 //		btnBook.setFocusPainted(false);			// 버튼 포커스 시 테두리 제거
 		
 		btnAirport = new JButton("항공");
-		btnAirport.setSize(150, 70);
-		btnAirport.setLocation(180, 0);
+		btnAirport.setSize(150, 40);
+//		btnAirport.setLocation(180, 0);
+		btnAirport.setLocation(350, 0);
 		btnAirport.addActionListener(this);
 		btnAirport.setFont(fontNanumGothic);
 		btnAirport.setBorderPainted(false);
 		btnAirport.setContentAreaFilled(false);
 		
 		btnInFight = new JButton("기내");
-		btnInFight.setSize(150, 70);
-		btnInFight.setLocation(350, 0);
+		btnInFight.setSize(150, 40);
+//		btnInFight.setLocation(350, 0);
+		btnInFight.setLocation(500, 0);
 		btnInFight.addActionListener(this);
 		btnInFight.setFont(fontNanumGothic);
 		btnInFight.setBorderPainted(false);
 		btnInFight.setContentAreaFilled(false);
 		
 		btnMyPage = new JButton("마이페이지");
-		btnMyPage.setSize(150, 70);
-		btnMyPage.setLocation(520, 0);
+		btnMyPage.setSize(150, 40);
+//		btnMyPage.setLocation(520, 0);
+		btnMyPage.setLocation(650, 0);
 		btnMyPage.addActionListener(this);
 		btnMyPage.setFont(fontNanumGothic);
 		btnMyPage.setBorderPainted(false);
@@ -103,7 +112,8 @@ public class MainForm extends JFrame implements ActionListener {
 		// 로그인 버튼
 		btnLogin = new JButton("<HTML><U>LOGIN</U></HTML>");
 		btnLogin.setSize(80, 50);
-		btnLogin.setLocation(970, 35);
+//		btnLogin.setLocation(970, 35);
+		btnLogin.setLocation(970, 5);
 		btnLogin.addActionListener(this);
 		btnLogin.setFont(fontArial);
 		btnLogin.setBorderPainted(false);
@@ -112,10 +122,12 @@ public class MainForm extends JFrame implements ActionListener {
 		pnTOP.add(btnLogo);
 		pnTOP.add(pnMenu);
 		
+		
 		pnMenu.add(btnBook);
 		pnMenu.add(btnAirport);
 		pnMenu.add(btnInFight);
 		pnMenu.add(btnMyPage);
+		
 		
 		pnTOP.add(btnLogin);
 		
@@ -133,5 +145,9 @@ public class MainForm extends JFrame implements ActionListener {
 	public void actionPerformed(ActionEvent e) {
 		Object obj = e.getSource();
 		
+		if(obj == btnLogin)
+		{
+			new loginForm("LOGIN", 400, 500, this);
+		}
 	}
 }
