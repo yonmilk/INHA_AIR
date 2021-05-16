65cd5a73f604674047e2d8a636b6c5542e15d38b
yewon-Noh <yewo2nn16@gmail.com>
2021-05-16 21:40:33 +0900

    basic form 추가

---
+++ src/Basic/BasicForm.java
@@ -0,0 +1,40 @@
+package Basic;
+import java.awt.Color;
+import java.awt.Container;
+import java.awt.Font;
+import java.awt.Menu;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+
+public class BasicForm extends JFrame implements ActionListener {
+	// Title 및 사이즈 설정
+	private String title = "INHA AIR";
+	private int width = 1120, height = 770;
+	
+	public BasicForm() {
+		setTitle(title);
+		setSize(width, height);
+		setResizable(false);
+		setLocationRelativeTo(this);
+		setDefaultCloseOperation(EXIT_ON_CLOSE);
+		
+		
+		setVisible(true);
+	}
+
+
+	public static void main(String[] args) {
+		new BasicForm();
+	}
+
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+
+	}
+}
