36cf9c4e2c19e635c85ea3957532d5219e620d8a
oow214 <83155697+oow214@users.noreply.github.com>
2021-05-17 23:56:04 +0900

    연우 - 달력 제목, 가는날 오는날 설정
    제목, 가는날, 오는날 설정

--- src/CalendarForm.java
+++ src/CalendarForm.java
@@ -11,6 +11,7 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.JTextField;
 
 import com.sun.org.apache.bcel.internal.generic.NEW;
 
@@ -20,19 +21,29 @@ import Basic.BasicForm;
 
 public class CalendarForm extends JFrame implements ActionListener {
 	
+	// Title 및 사이즈 설정
+	private String title = "탑승일 선택";
+	private int width = 800, height = 700;
+	
 	//private MainForm mainForm;
 
 	//폰트
 	Font fontGothic = new Font("Gothic", Font.BOLD, 20);				// 고딕
 	Font fontNanumGothic18 = new Font("NanumGothic", Font.BOLD, 18);	// 나눔고딕 18
-	Font fontNanumGothic25 = new Font("NanumGothic", Font.BOLD, 25);	// 나눔고딕 25
+	Font fontNanumGothic20 = new Font("NanumGothic", Font.BOLD, 20);	// 나눔고딕 25
 	Font fontArial = new Font("Arial", Font.PLAIN, 12);					// 영어
 	private JPanel jpTitle, jpSelect, jpCalendar, jpBtn;
 	private JLabel lblTitle;
+	private JLabel lblCome;
+	private JLabel lblGo;
+	private JTextField tfGo;
+	private JTextField tfCome;
+	private JButton btnReselect;
+	private JButton btnSelect;
 
 	
 
-	public CalendarForm(String title, int width, int height) {
+	public CalendarForm() {
 			
 		//this.mainForm = mainForm;
 		
@@ -66,28 +77,54 @@ public class CalendarForm extends JFrame implements ActionListener {
 		jpTitle.setBackground(Color.white);
 		//제목라벨
 		lblTitle = new JLabel("탑승일 선택");
-		lblTitle.setFont(fontNanumGothic25);
+		lblTitle.setFont(fontNanumGothic20);
 		lblTitle.setSize(150, 40);
 		lblTitle.setLocation(10, 0);
 		jpTitle.add(lblTitle);
 		
-		//날짜표시판넬
-		jpSelect = new JPanel();
+		//가는날, 오는날
+		jpSelect = new JPanel(); //날짜표시 판매
 		jpSelect.setLayout(null);
-		jpSelect.setSize(650, 50);
+		jpSelect.setSize(650, 40);
 		jpSelect.setLocation(50, 100);
-		jpSelect.setBackground(Color.red);
-		//가는날 라벨, 
+		jpSelect.setBackground(Color.white);
 		
+		lblGo = new JLabel("가는 날"); //가는날 라벨
+		lblGo.setFont(fontNanumGothic18);
+		lblGo.setSize(150, 40);
+		lblGo.setLocation(10, 0);
+
+		tfGo = new JTextField(); //가는 날짜 선택 시 확인
+		tfGo.setFont(fontNanumGothic18);
+		tfGo.setSize(150, 40);
+		tfGo.setLocation(90,0);
 		
+		lblCome = new JLabel("오는 날 "); //오는날 라벨
+		lblCome.setFont(fontNanumGothic18);
+		lblCome.setSize(150, 40);
+		lblCome.setLocation(375, 0);
+		tfCome = new JTextField(); //오는 날짜 선택 시 확인
+		tfCome.setFont(fontNanumGothic18);
+		tfCome.setSize(150, 40);
+		tfCome.setLocation(455, 0);
+		
+		jpSelect.add(lblGo);
+		jpSelect.add(tfGo);
+		jpSelect.add(lblCome);
+		jpSelect.add(tfCome);
 		
 		jpCalendar = new JPanel();
 		setCalendar();
 		
 		
-		//하단 버튼 판넬
-		jpBtn = new JPanel();
+		//다시선택, 왕복/편도 선택
+		jpBtn = new JPanel(); //하단 버튼 판넬
+		btnReselect = new JButton("다시 선택"); //다시 선택 버튼
+		btnSelect = new JButton("편도 선택"); //왕복/편도 선택 버튼
 		
+		
+		//jpBtn.add(btnReselect);
+		//jpBtn.add(btnSelect);
 		
 		add(jpTitle);
 		add(jpSelect);
@@ -99,7 +136,7 @@ public class CalendarForm extends JFrame implements ActionListener {
 
 
 	public static void main(String[] args) {
-		new CalendarForm("탑승일 선택", 800, 700);
+		new CalendarForm();
 	}	
 	
 	
