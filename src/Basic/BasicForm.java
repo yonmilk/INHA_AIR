f3513ae37d0a453070306e53086bd5e6d3d117b1
yewon-Noh <yewo2nn16@gmail.com>
2021-05-17 14:03:10 +0900

    예원 - 기본 틀

--- src/Basic/BasicForm.java
+++ src/Basic/BasicForm.java
@@ -16,6 +16,8 @@ public class BasicForm extends JFrame implements ActionListener {
 	private String title = "INHA AIR";
 	private int width = 1120, height = 770;
 	
+	private JPanel pnTOP;
+	
 	public BasicForm() {
 		setTitle(title);
 		setSize(width, height);
@@ -23,8 +25,24 @@ public class BasicForm extends JFrame implements ActionListener {
 		setLocationRelativeTo(this);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		
+		// 이거 건들지말기
+		setLayout(null);
+		setUpMenu();
 		
 		setVisible(true);
+	}
+
+
+	// 이거 건들지말기
+	private void setUpMenu() {
+		// 상단 판넬
+		pnTOP = new JPanel();		// 상단 판넬 생성
+		pnTOP.setLayout(null);		// 상단 판넬 배치관리자 설정
+		pnTOP.setSize(1120, 80);	// 상단 판넬 사이즈 설정
+		pnTOP.setLocation(0, 0);	// 상단 판넬 위치 설정
+		pnTOP.setBackground(Color.black);	// 상단 패널 배경색 설정
+		
+		add(pnTOP);
 	}
 
 
