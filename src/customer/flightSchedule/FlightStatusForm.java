b8a7257c2d25e797af29d9f409cd0a84e356a03a
yewon-Noh <yewo2nn16@gmail.com>
2021-06-12 23:32:00 +0900

    예원 - 완

--- src/customer/flightSchedule/FlightStatusForm.java
+++ src/customer/flightSchedule/FlightStatusForm.java
@@ -78,12 +78,12 @@ public class FlightStatusForm extends JFrame implements ActionListener {
 	// 예원 - 시작 화면
 	public FlightStatusForm() {
 		
-		// DB 정보 - 테스트 소스
-		String dbURL="jdbc:mysql://114.71.137.174:61083/inhaair?serverTimezone=UTC&useSSL=false";
-		String dbID="inhaair";
-		String dbPassword="1234";
-		// 데이터베이스 연결 - 테스트 소스
-		databaseClass.connect(dbURL, dbID, dbPassword);
+//		// DB 정보 - 테스트 소스
+//		String dbURL="jdbc:mysql://114.71.137.174:61083/inhaair?serverTimezone=UTC&useSSL=false";
+//		String dbID="inhaair";
+//		String dbPassword="1234";
+//		// 데이터베이스 연결 - 테스트 소스
+//		databaseClass.connect(dbURL, dbID, dbPassword);
 		
 		// 공항 정보 가져오기
 		setCity();
